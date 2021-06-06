783f9e9493ab6c1aa8da98d26f42264e2647f208
eleftheros <eleftheros_all@protonmail.com>
2021-06-06 19:04:25 +0300

    Add checkPalindrome function

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,34 @@
 const palindromeForm = document.getElementById("palindromeForm");
+const palindromeResult = document.getElementById("palindromeResult");
 
 // function to check if input is a palindrome
+const checkPalindrome = string => {
+  let output = "";
+  string.split("").forEach(letter => {
+    output = letter + output;
+  })
+
+  //check if string and palindrome are equal
+  if(output === string) {
+    palindromeResult.className = "container__result success";
+    palindromeResult.textContent = `${string.toUpperCase()} is a PALINDROME!`;
+  } else {
+    palindromeResult.className = "container__result error";
+    palindromeResult.textContent = `${string.toUpperCase()} is NOT a palindrome!`;
+  }
+};
 
 // function to submit palindromeForm
 const submitPalindromeForm = e => {
   //prevent default form action
   e.preventDefault();
 
+  palindromeResult.className = "container__result";
   let value = e.target.palindromeInput.value;
   value = value.trim().toLowerCase();
   
   if(value)
-  // checkPalindrome(value);
+    checkPalindrome(value);
 
   //reset form
   e.target.reset();
